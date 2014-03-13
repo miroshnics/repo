c09f7655a31adfe02bf0102b465c1cf7f5e518ce
miroshni.cs <miroshni.cs@gmail.com>
2014-03-14 00:37:10 +0400

    modified:   index.php 	new file:   uninit.php

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <html>
 <head><title>Онлайн-Диспетчер автопарка</title></head>
-<body>
-<span>
+<body><? require 'init.php'; ?>
+
+  <br />
 <? 
-echo "Hello world";
-echo 5+5;
+
+    /*print "Connected successfully" . "</br>";
+    mysql_select_db("my_database") or die("К сожалению, не доступна база данных.</br>");
+*/
+    $ver = mysql_query("SELECT VERSION()"); 
+  if(!$ver) 
+  { 
+    echo "<p>Ошибка в запросе</p>"; 
+    exit(); 
+  } 
+  echo "MySQL version is " . mysql_result($ver, 0);
+
+  $data = mysql_query("SELECT CURRENT_DATE;"); 
+  echo mysql_result($data, 0); 
 ?>
-</span>
-</body>
+
+<?
+
+    /* Выполняем SQL-запрос */
+    $query = "SELECT * FROM my_table";
+    $result = mysql_query($query) or die("Неверный запрос: " . mysql_error() . "</br>");
+
+    /* Выводим результаты в html */
+    print "<table>\n";
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        print "\t<tr>\n";
+        foreach ($line as $col_value) {
+            print "\t\t<td>$col_value</td>\n";
+        }
+        print "\t</tr>\n";
+    }
+    print "</table>\n";
+?>
+
+<? require ('uninit.php'); ?></body>
 </html>
--- init.php
+++ init.php
@@ -1,25 +1,12 @@
-<?php 
-  $dblocation = "127.0.0.1"; 
+
+<?
+  $dblocation = "localhost"; 
   $dbname = "test"; 
   $dbuser = "root"; 
   $dbpasswd = "12345"; 
 
-  $dbcnx = @mysql_connect($dblocation, $dbuser, $dbpasswd); 
-  if (!$dbcnx) 
-  { 
-    echo "<p>К сожалению, не доступен сервер mySQL</p>"; 
-    exit(); 
-  } 
-  if (!@mysql_select_db($dbname,$dbcnx) ) 
-  { 
-    echo "<p>К сожалению, не доступна база данных</p>"; 
-    exit(); 
-  } 
-  $ver = mysql_query("SELECT VERSION()"); 
-  if(!$ver) 
-  { 
-    echo "<p>Ошибка в запросе</p>"; 
-    exit(); 
-  } 
-  echo mysql_result($ver, 0); 
-?> 
+      /* Соединяемся, выбираем базу данных */
+    $link = mysql_connect($dblocation, $dbuser, $dbpasswd)
+        or die("Невозможно подключиться к базе данных: " . mysql_error());
+
+  ?>
