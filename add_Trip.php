2a279d25150f24a2bc4da32466f990a17c9cf1c8
miroshni.cs <miroshni.cs@gmail.com>
2014-03-27 00:15:07 +0400

    added gateway php to mysql

--- add_Trip.php
+++ add_Trip.php
@@ -3,16 +3,15 @@ if(!$link) die("Невозможно подключиться к серверу СУБД: " . mysql_error());
 
 /* Добавляем в БД новую поездку */
 $str = "INSERT 
-		INTO tbl_Trips (end_point, time_start, time_end, Driver_id, client_dept_id, client, dlina) 
+		INTO tbl_Trips (end_point, time_start, Driver_id, client_dept_id, client, dlina, time_end) 
 		VALUES( 
 		'{$_POST['end_point']}' ,
-		'{$_POST['sql_date'] . $_POST['time_start']}' ,
-		'{$_POST['last_name']}',
-		'{$_POST['car']}',
-		'{$fuel_id}');";
+		'{$_POST['sql_date']} {$_POST['time_start']}' ,
+		'{$_POST['Driver_id']}',
+		'{$_POST['client_dept_id']}',
+		'{$_POST['client']}',
+		'{$_POST['dlina']}',
+		'{$_POST['time_end']}');";
 
-
-if (!mysql_query($str)) echo 'Ошибка при добавлении в БД нового водителя: ' . mysql_error() . "<br />";
-
-echo $_POST['end_point'] . $_POST['date_start'] . $_POST['time_start'] . $_POST['Driver_id'] . "<br />";
+mysql_query($str) or die('Ошибка при добавлении в БД нового водителя: ' . mysql_error() . "<br />");
 ?>
